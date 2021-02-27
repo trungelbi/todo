8daa9201541bb9da0e57ab68a4569b6555f177f8
Trung Ngo Bao <trungngobao@TrungnoMacBook-Pro.local>
2021-02-27 12:04:59 +0900

    add to add todo list, add todo items to list, and delete todo items

---
+++ db/migrate/20210225081913_create_todo_lists.rb
@@ -0,0 +1,11 @@
+class CreateTodoLists < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todo_lists do |t|
+      t.string :title
+      t.text :description
+
+      t.timestamps
+    end
+  end
+  has_many :todo_items
+end
